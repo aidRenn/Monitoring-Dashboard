1450a90ad0fb43b4c79ade703112f5059430adec
aidRenn <muhammad.said010903@gmail.com>
2026-02-10 15:44:06 +0800

    Fix layout

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function updateClock() {
 setInterval(updateClock, 1000);
 updateClock(); // Initial call
 
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+    const header = item.querySelector(".menu-header");
+
+    header.addEventListener("click", () => {
+
+        if (item.classList.contains("expanded")) {
+            item.classList.remove("expanded");
+            item.classList.add("collapsed");
+        } else {
+
+            // Tutup semua
+            menuItems.forEach(i => {
+                i.classList.remove("expanded");
+                i.classList.add("collapsed");
+            });
+
+            // Buka yang diklik
+            item.classList.remove("collapsed");
+            item.classList.add("expanded");
+        }
+    });
+});
